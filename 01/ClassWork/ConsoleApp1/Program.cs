737ede67280328463ee44aa0bc50db766eebe1f1
geekbrains <info@geekbrains.ru>
2021-04-19 22:43:43 +0300

    class work #1 added

---
+++ 01/ClassWork/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Text;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            Console.WriteLine("Введите имя: ");
+            string name = Console.ReadLine();
+            Thread.Sleep(5000);
+            Console.WriteLine($"Здравствуйте, {name}!");
+            Console.ReadKey();
+            
+            // string line = Console.ReadLine();
+
+
+            Console.WriteLine("Привет!");
+        }
+    }
+}
