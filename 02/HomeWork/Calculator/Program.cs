3f0618067aa9d49664b0b5cb4ad97c24408c07b0
geekbrains <info@geekbrains.ru>
2021-04-22 00:17:58 +0300

    added homework

---
+++ 02/HomeWork/Calculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace Calculator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.InputEncoding = Encoding.Unicode;  // support unicode for input
+			Console.OutputEncoding = Encoding.Unicode; // support unicode for output
+			Console.Write("Введите первое число: ");
+			string fromConsole1 = Console.ReadLine();
+			float number1 = float.Parse(fromConsole1);
+			Console.Write("Введите второе число: ");
+			string fromConsole2 = Console.ReadLine();
+			float number2 = float.Parse(fromConsole2);
+			float sum = number1 + number2;
+			float sub = number1 - number2;
+			float mul = number1 * number2;
+			float dev = number1 / number2;
+			Console.WriteLine($"Сумма двух чисел= {sum}");
+			Console.WriteLine($"Разность двух чисел= {sub}");
+			Console.WriteLine($"Произведение двух чисел= {mul}");
+			if (number2==0)
+				Console.WriteLine("На ноль делить нельзя!");
+			else
+				Console.WriteLine($"Частное двух чисел= {dev}");
+		}
+		}
+	}
+
