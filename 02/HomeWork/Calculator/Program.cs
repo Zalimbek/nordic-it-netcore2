0672c2d0c1968fb2c86a5d214e112a4043e39876
geekbrains <info@geekbrains.ru>
2021-04-25 18:59:18 +0300

    changed if input is 0 logic

--- 02/HomeWork/Calculator/Program.cs
+++ 02/HomeWork/Calculator/Program.cs
@@ -9,24 +9,49 @@ namespace Calculator
 		{
 			Console.InputEncoding = Encoding.Unicode;  // support unicode for input
 			Console.OutputEncoding = Encoding.Unicode; // support unicode for output
-			Console.Write("Введите первое число: ");
-			string fromConsole1 = Console.ReadLine();
-			float number1 = float.Parse(fromConsole1);
-			Console.Write("Введите второе число: ");
-			string fromConsole2 = Console.ReadLine();
-			float number2 = float.Parse(fromConsole2);
-			float sum = number1 + number2;
-			float sub = number1 - number2;
-			float mul = number1 * number2;
-			float dev = number1 / number2;
-			Console.WriteLine($"Сумма двух чисел= {sum}");
-			Console.WriteLine($"Разность двух чисел= {sub}");
-			Console.WriteLine($"Произведение двух чисел= {mul}");
-			if (number2==0)
-				Console.WriteLine("На ноль делить нельзя!");
+			// defining parameters sum, sub, mul, dev
+
+			float sum = 0;
+			float sub = 0;
+			float mul = 0;
+			float dev = 0;
+			
+			// Enter first value and check if it is 0
+			CheckPoint1:
+			Console.Write("Введите первое число, отличное от нуля: ");
+			string userInput1 = Console.ReadLine();
+			float number1 = float.Parse(userInput1);
+
+			if (number1 == 0)
+			{
+				Console.WriteLine("Введите число, отличное от нуля!");
+				goto CheckPoint1;
+			}
 			else
-				Console.WriteLine($"Частное двух чисел= {dev}");
-		}
+			{
+			CheckPoint2:
+			// Enter second value and check if it is 0
+				Console.Write("Введите второе число, отличное от нуля: ");
+				string userInput2 = Console.ReadLine();
+				float number2 = float.Parse(userInput2);
+				if (number2 == 0)
+				{
+					Console.WriteLine("Введите число, отличное от нуля!");
+					goto CheckPoint2;
+				}
+				else
+				{
+					sum = number1 + number2;
+					sub = number1 - number2;
+					mul = number1 * number2;
+					dev = number1 / number2;
+
+					Console.WriteLine($"Сумма двух чисел= {sum}");
+					Console.WriteLine($"Разность двух чисел= {sub}");
+					Console.WriteLine($"Произведение двух чисел= {mul}");
+					Console.WriteLine($"Частное двух чисел= {dev}");
+				}
+			}
 		}
 	}
-
+}
