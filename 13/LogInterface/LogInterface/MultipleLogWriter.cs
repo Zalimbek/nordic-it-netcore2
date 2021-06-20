69a989c6ee2f77911cb78a67173125068af32072
geekbrains <info@geekbrains.ru>
2021-06-21 00:55:40 +0300

    partia;;y done

--- 13/LogInterface/LogInterface/MultipleLogWriter.cs
+++ 13/LogInterface/LogInterface/MultipleLogWriter.cs
@@ -7,23 +7,11 @@ namespace LogInterface
 	class MultipleLogWriter:ILogWriter
 	{
 
-		public MultipleLogWriter(ILogWriter info, ILogWriter error, ILogWriter warning)//-что значит принимать коллекцию классов? непосредственно сами методы?
+		public MultipleLogWriter(FileLogWriter fileLog, ConsoleLogWriter consoleLog)//-что значит принимать коллекцию классов? непосредственно сами методы?
 		{
-			LogInfo = info;
+			FileLogWriter = fileLog;
+			ConsoleLogWriter = consoleLog;
 
 		}
 
-		public void LogInfo(string message)
-		{
-			Console.WriteLine("{0:yyyy-MM-ddThh:mm:ss+0000}\tInfo\t{1}", DateTime.UtcNow, message);
-		}
-		public void LogError(string message)
-		{
-
-			Console.WriteLine("{0:yyyy-MM-ddThh:mm:ss+0000}\tError\t{1}", DateTime.UtcNow, message);
-		}
-		public void LogWarning(string message)
-		{
-			Console.WriteLine("{0:yyyy-MM-ddThh:mm:ss+0000}\tWarning\t{1}", DateTime.UtcNow, message);
-		}
 }
