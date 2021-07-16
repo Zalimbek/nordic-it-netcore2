cf5aec21569e20b50915ebdf20a6cf84c2257716
geekbrains <info@geekbrains.ru>
2021-07-16 15:38:38 +0300

    added ClassWork

---
+++ 21/DemoEmptyWebApplication/RequestPipelineEmulation/PipelineMiddleWare.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RequestPipelineEmulation
+{
+	public class PipelineMiddleWare
+	{
+		public string Name;
+		public PipelineMiddleWare NextMiddleWare { get; set; }
+
+		public Func<string, string> PreAction { get; set; }
+		public Func<string, string> PostAction { get; set; }
+
+		public string Run(string request)
+		{
+			Console.WriteLine($"Running {Name} started...");
+			string request1 = PreAction?.Invoke(request);
+			string request2 = NextMiddleWare?.Run(request1);
+			string request3 = PostAction?.Invoke(request2);
+			Console.WriteLine($"Running {Name} finished...");
+			return request3;
+		}
+	}
+}
---
+++ 21/DemoEmptyWebApplication/RequestPipelineEmulation/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace RequestPipelineEmulation
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			PipelineMiddleWare requestHandler = new PipelineMiddleWare
+			{
+				Name = "requestHandler",
+				PreAction = (request) =>
+				{
+					return request;
+				},
+
+				PostAction = (response) =>
+				{
+					return response;
+				}
+			};
+
+			PipelineMiddleWare authHandler = new PipelineMiddleWare
+			{
+				Name = "authHandler",
+				NextMiddleWare = requestHandler,
+				PreAction = (request) => { if (!request.StartsWith("from: zhamilia"))
+						return "403:unauthorized";
+					else
+						return request;
+				},
+				PostAction = (response) =>
+				{
+					return response;
+				}
+			};
+
+			PipelineMiddleWare ExceptionHandler = new PipelineMiddleWare
+			{
+				Name = "ExceptionHandler",
+				NextMiddleWare = authHandler,
+				PreAction = (request) =>
+				{
+					return request;
+				},
+
+				PostAction = (response) =>
+				{
+					return "data";
+				}
+
+			};
+
+			string request = "from: zhamilia";
+			string response=ExceptionHandler.Run(request);
+		}
+	}
+}
